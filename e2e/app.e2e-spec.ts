49b5656698124949823ca433ee189eb137fbb6bb
joan.tolos <joan.tolos@clarivate.com>
2018-01-05 14:09:26 +0100

    End to end tests updated

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('angular-tour-of-heroes App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+    expect(page.getParagraphText()).toEqual('Tour of Heroes');
   });
 });
