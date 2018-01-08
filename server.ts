cacaea1b3ede13823802aae4cb8039804d8611c9
joan.tolos <joan.tolos@clarivate.com>
2018-01-08 10:49:54 +0100

    Changing server.js to server.ts

--- server.ts
+++ server.ts
@@ -7,7 +7,7 @@ app.use(express.static(__dirname + '/dist'));
 app.listen(process.env.PORT || 8080);
 
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(_dirname + '/dist/index.html'))
+  res.sendFile(path.join(__dirname + '/dist/index.html'))
 });
 
 console.log('Console listening!');
